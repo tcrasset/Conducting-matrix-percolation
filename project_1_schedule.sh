5d888addd9abcdfa9bc98d37c56dafef8c2ceec1
tcrasset <tomcrasset@gmail.com>
2019-10-08 14:03:04 +0200

    Fix error in scripts 2

--- project_1_schedule.sh
+++ project_1_schedule.sh
@@ -9,7 +9,7 @@ do
         #Taking M = 1000
         for chunk_size in $((1000/$threads)) $((512/$threads)) $((256/$threads)) $((128/$threads)) 8 4 2 1
         do
-            filename="project1_out_$threads_$schedule_type-$chunk_size.txt"
+            filename="project1_out_schedule.txt"
             echo "Threads: $threads Schedule: '$schedule_type, $chunk_size' output: $filename"
             sbatch --cpus-per-task=$threads schedule_test.sh $filename $schedule_type $chunk_size
         done
--- schedule_test.sh
+++ schedule_test.sh
@@ -16,12 +16,11 @@
 filename=$1
 schedule_type=$2
 chunk_size=$3
-rm $filename
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_SCHEDULE="$schedule_type,$chunk_size"
 
-echo "Flag N d M NbThreads AvgTime Schedule_kind Chunk_size Probability" >> $filename
+echo "Flag N d M NbThreads AvgTime Schedule_kind Chunk_size Probability" > $filename
 for d in $(seq 0 0.1 1);
     do
         ./conducting 1 100 $d 1000 >> $filename
